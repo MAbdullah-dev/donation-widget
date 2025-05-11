7522ee98db48c3bda3d304f63113538c12382d01
root <root@ip-172-31-5-118.ap-southeast-2.compute.internal>
2025-05-11 20:16:17 +0000

    WIP: local changes to welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>donation widget</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <link rel="stylesheet" href="{{ asset('CSS/Header.css') }}">
     <link rel="stylesheet" href="{{ asset('CSS/Globalstyle.css') }}"> --}}
     @vite(['resources/css/app.css'])
